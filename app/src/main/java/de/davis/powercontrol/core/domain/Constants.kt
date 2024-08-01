59597b560e2a10d45088c161a87de637917feb75
Davis <davis-wolfermann2005@gmx.de>
2024-08-01 14:16:01 +0200

    [Core] Add operation bytes

--- app/src/main/java/de/davis/powercontrol/core/domain/Constants.kt
+++ app/src/main/java/de/davis/powercontrol/core/domain/Constants.kt
@@ -2,3 +2,8 @@ package de.davis.powercontrol.core.domain
 
 const val DEFAULT_SHUTDOWN_SEQUENCE = 0xa5.toByte()
 const val DEFAULT_PORT = 7848
+
+const val HEARTBEAT_BYTE = 0x00.toByte()
+const val SHUTDOWN_BYTE = 0x01.toByte()
+const val RESTART_BYTE = 0x02.toByte()
+const val LOGOUT_BYTE = 0x03.toByte()
