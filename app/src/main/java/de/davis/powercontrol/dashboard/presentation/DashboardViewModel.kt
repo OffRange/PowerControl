34e60a2dcb7328cfa5e20fd3f497a9aa68d29b21
Davis <davis-wolfermann2005@gmx.de>
2024-08-01 17:59:39 +0200

    [Dashboard] Refactor viewmodel to support device status

--- app/src/main/java/de/davis/powercontrol/dashboard/presentation/DashboardViewModel.kt
+++ app/src/main/java/de/davis/powercontrol/dashboard/presentation/DashboardViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import de.davis.powercontrol.core.domain.models.DeviceStatus
 import de.davis.powercontrol.core.domain.`typealias`.IpAddress
+import de.davis.powercontrol.core.domain.usecases.GetDeviceStatusUseCase
 import de.davis.powercontrol.core.domain.usecases.ShutdownUseCase
 import de.davis.powercontrol.core.domain.usecases.WakeOnLanUseCase
 import de.davis.powercontrol.device.domain.repository.DeviceRepository
@@ -21,16 +22,43 @@ import kotlinx.coroutines.launch
 class DashboardViewModel(
     private val shutdown: ShutdownUseCase,
     private val boot: WakeOnLanUseCase,
+    private val getDeviceStatus: GetDeviceStatusUseCase,
     private val deviceRepository: DeviceRepository
 ) : ViewModel() {
 
-    val devices = deviceRepository.observeAllDevices().distinctUntilChanged().stateIn(
+    private val _noStatusDevices =
+        deviceRepository.observeAllDevices().distinctUntilChanged().stateIn(
+            viewModelScope, SharingStarted.Eagerly, emptyList()
+        )
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val devices = _noStatusDevices.flatMapLatest { deviceList ->
+        val statusFlows = deviceList.map { device ->
+            flow {
+                emit(device.copy(status = DeviceStatus.Pending))
+                while (viewModelScope.isActive) {
+                    val status = getDeviceStatus(device)
+                    emit(device.copy(status = status))
+
+                    delay(5000)
+                }
+            }.distinctUntilChanged()
+        }
+        combine(statusFlows) { it.toList() }
+    }.stateIn(
         viewModelScope, SharingStarted.Eagerly, emptyList()
     )
 
     fun bootOrShutdown(ip: IpAddress) {
         viewModelScope.launch {
-            deviceRepository.getDeviceByIp(ip)?.let { shutdown(it) }
+            deviceRepository.getDeviceByIp(ip)?.let {
+                val status = getDeviceStatus(it)
+                when (status) {
+                    DeviceStatus.Online -> shutdown(it)
+                    DeviceStatus.Offline -> boot(it)
+                    else -> {}
+                }
+            }
         }
     }
 }
