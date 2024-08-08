aefdf627e5063810c34383477907f72b9c7104a8
Davis <davis-wolfermann2005@gmx.de>
2024-08-08 22:37:05 +0200

    [Gradle] Add work api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,8 @@ dependencies {
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
 
+    implementation(libs.androidx.work.runtime.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
