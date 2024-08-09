42c254f97fc480dbbfa804af1b550a873bd0fcdb
Davis <davis-wolfermann2005@gmx.de>
2024-08-09 18:23:28 +0200

    [Core] Remove ShutdownOrBoot type

--- app/src/main/java/de/davis/powercontrol/core/domain/models/Operation.kt
+++ app/src/main/java/de/davis/powercontrol/core/domain/models/Operation.kt
@@ -1,10 +1,6 @@
 package de.davis.powercontrol.core.domain.models
 
-sealed interface Operation {
-    data object ShutdownOrBoot : Operation
-}
-
-enum class PowerOperation : Operation {
+enum class PowerOperation {
     Shutdown,
     Boot,
     Restart,
